0585e2157d3ba5c7fce4a60db8992531f76b0994
henrique-diomede <henrique.diomede@hpe.com>
2016-06-02 10:26:27 -0300

    Enclosure add and remove

---
+++ resources/oneview_enclosure.rb
@@ -0,0 +1,21 @@
+Opscode::OneviewResourceBaseProperties.load(self)
+
+resource_name :oneview_enclosure
+
+property :enclosure_group
+
+action_class do
+  include Opscode::OneviewHelper
+  include Opscode::OneviewResourceBase
+end
+
+action :add do
+  item = load_resource
+  eg = OneviewSDK::EnclosureGroup.new(item.client, name: new_resource.enclosure_group)
+  item.set_enclosure_group(eg)
+  create_or_update(item)
+end
+
+action :remove do
+  delete
+end
