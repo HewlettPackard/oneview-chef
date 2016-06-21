2688bdc55a88dc77f8a0bacb0fbcb09484c72d1a
Jared Smartt <jared.smartt@hpe.com>
2016-06-21 08:28:42 -0600

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -18,8 +18,8 @@ long_description 'Provides OneView resources'
 
 version          '0.1.0'
 
-source_url       'https://github.com/HewlettPackard/chef-oneview' if respond_to?(:source_url)
-issues_url       'https://github.com/HewlettPackard/chef-oneview/issues' if respond_to?(:issues_url)
+source_url       'https://github.com/HewlettPackard/oneview-chef' if respond_to?(:source_url)
+issues_url       'https://github.com/HewlettPackard/oneview-chef/issues' if respond_to?(:issues_url)
 
 chef_version     '>= 12' if respond_to?(:chef_version)
 depends          'compat_resource'
