a92f2ecde3a6119f9d4ab58a7c607bd09c4b3b86
thiago-mio-amaral <thiago.mio.amaral@hpe.com>
2016-08-11 17:11:39 -0300

    fixed class method call mock

--- spec/unit/resources/ethernet_network/bulk_create_spec.rb
+++ spec/unit/resources/ethernet_network/bulk_create_spec.rb
@@ -5,7 +5,7 @@ describe 'oneview_test::ethernet_network_bulk_create' do
   include_context 'chef context'
 
   it 'bulk creates it' do
-    expect_any_instance_of(OneviewSDK::EthernetNetwork).to receive(:bulk_create).and_return(true)
+    expect(OneviewSDK::EthernetNetwork).to receive(:bulk_create).and_return(true)
     expect(real_chef_run).to bulk_create_oneview_ethernet_network('BulkEthernetNetwork')
   end
 end
