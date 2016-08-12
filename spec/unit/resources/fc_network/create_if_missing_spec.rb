12e0f99851e90aad4d36180304a3348c78e4ec05
thiago-mio-amaral <thiago.mio.amaral@hpe.com>
2016-08-12 09:49:49 -0300

    Added tests for FCNetwork

---
+++ spec/unit/resources/fc_network/create_if_missing_spec.rb
@@ -0,0 +1,19 @@
+require_relative './../../../spec_helper'
+
+describe 'oneview_test::fc_network_create_if_missing' do
+  let(:resource_name) { 'fc_network' }
+  include_context 'chef context'
+
+  it 'creates it when it does not exist' do
+    expect_any_instance_of(OneviewSDK::FCNetwork).to receive(:exists?).and_return(false)
+    expect_any_instance_of(OneviewSDK::FCNetwork).to receive(:create).and_return(true)
+    expect(real_chef_run).to create_oneview_fc_network_if_missing('FCNetwork2')
+  end
+
+  it 'does nothing when it exists' do
+    expect_any_instance_of(OneviewSDK::FCNetwork).to receive(:exists?).and_return(true)
+    expect_any_instance_of(OneviewSDK::FCNetwork).to receive(:retrieve!).and_return(true)
+    expect_any_instance_of(OneviewSDK::FCNetwork).to_not receive(:create)
+    expect(real_chef_run).to create_oneview_fc_network_if_missing('FCNetwork2')
+  end
+end
