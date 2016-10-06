c5caa1849ae4cd3c462fbc25fe6f8e72abee45ab
thiago-mio-amaral <thiago.mio.amaral@hpe.com>
2016-10-06 16:58:42 -0300

    Fixed updates and add/remove interconnect

--- resources/logical_interconnect.rb
+++ resources/logical_interconnect.rb
@@ -20,49 +20,47 @@ default_action :none
 action_class do
   include OneviewCookbook::Helper
   include OneviewCookbook::ResourceBase
-  def interconnect_handler(present_block, absent_block)
-    item = load_resource
+  def interconnect_handler(present_block, absent_block, item = nil)
+    item ||= load_resource
     raise "Unspecified property: 'bay_number'. Please set it before attempting this action." unless bay_number
     raise "Unspecified property: 'enclosure'. Please set it before attempting this action." unless enclosure
-    interconnect_list = OneviewSDK::Interconnect.find_by(@client, {})
-    # Procedure to get (bay, enclosure) pairs
-    get_pair = Proc.new do |inter|
-      bayn = inter['interconnectLocation']['locationEntries'].select { |e| e['type'] == 'Bay' }['value']
-      encn = inter['interconnectLocation']['locationEntries'].select { |e| e['type'] == 'Enclosure' }['value']
-      [bayn, encn]
+    interconnect_list = OneviewSDK::Interconnect.find_by(item.client, {})
+    enclosure_item = OneviewSDK::Enclosure.find_by(item.client, name: enclosure).first
+    # Procedure to get (bay, enclosure) pairs and interconnect state
+    listed_pairs = {}
+    interconnect_list.each do |inter|
+      bayn = inter['interconnectLocation']['locationEntries'].select { |e| e['type'] == 'Bay' }.first['value']
+      encn = inter['interconnectLocation']['locationEntries'].select { |e| e['type'] == 'Enclosure' }.first['value']
+      listed_pairs[[bayn, encn]] = inter['state']
     end
-    enclosure_item = OneviewSDK::Enclosure.find_by(item.client, name: enclosure)
-    # Checks if the list contains an pair that matches the one described
-    if interconnect_list.collect(&:get_pair).include?([bay_number, enclosure_item['uri'])
-      present_block.call
+    # Desired (bay, enclosure) interconnect location
+    pair = [bay_number.to_s, enclosure_item['uri']]
+    # If an interconnect exists in the location, and its state is not 'Absent', so it is present
+    if listed_pairs.keys.include?(pair) && listed_pairs[pair] != 'Absent'
+      present_block.call(bay_number, enclosure_item)
     else
-      absent_block.call
+      absent_block.call(bay_number, enclosure_item)
     end
   end
 
   def update_handler(action, key = nil, item = nil, temp = nil)
     item ||= load_resource
     temp ||= if key
-               key.to_s => Marshal.load(Marshal.dump(item[key]))
+               { key.to_s => Marshal.load(Marshal.dump(item[key])) }
              else
                Marshal.load(Marshal.dump(item.data))
              end
-    if item.exists?
-      item.retrieve!
-      if item.like? temp
-        Chef::Log.info("#{resource_name} '#{name}' is up to date")
-      else
-        Chef::Log.info "#{action.to_s.capitalize.gsub('_', ' ')} #{resource_name} '#{name}'"
-        Chef::Log.debug "#{resource_name} '#{name}' Chef resource differs from OneView resource."
-        Chef::Log.debug "Current state: #{JSON.pretty_generate(item.data)}"
-        Chef::Log.debug "Desired state: #{JSON.pretty_generate(temp)}"
-        diff = get_diff(item, temp)
-        converge_by "#{action.to_s.capitalize.gsub('_', ' ')} #{resource_name} '#{name}'#{diff}" do
-          item.update(temp)
-        end
-      end
-    else
-      raise "Resource not found: Action '#{action}' cannot be performed since #{resource_name} '#{name}' was not found."
+    raise "Resource not found: Action '#{action}' cannot be performed since #{resource_name} '#{name}' was not found." unless item.exists?
+    item.retrieve!
+    return Chef::Log.info("#{resource_name} '#{name}' is up to date") if item.like?(temp)
+    Chef::Log.info "#{action.to_s.capitalize.tr('_', ' ')} #{resource_name} '#{name}'"
+    Chef::Log.debug "#{resource_name} '#{name}' Chef resource differs from OneView resource."
+    Chef::Log.debug "Current state: #{JSON.pretty_generate(item.data)}"
+    Chef::Log.debug "Desired state: #{JSON.pretty_generate(temp)}"
+    diff = get_diff(item, temp)
+    temp[key].each { |k, v| item[key][k] = v } if key
+    converge_by "#{action.to_s.capitalize.tr('_', ' ')} #{resource_name} '#{name}'#{diff}" do
+      item.send(action)
     end
   end
 end
@@ -70,24 +68,30 @@ end
 action :none do
 end
 
-action :create_interconnect do
-  noaction = { Chef::Log.info("Interconnect already created in #{enclosure} at bay #{bay_number}") }
-  do_create = do
-    converge_by "Create interconnect in #{enclosure} at bay #{bay_number}"
-      item.create(bay_number, enclosure_item)
+action :add_interconnect do
+  item = load_resource
+  noaction = proc do
+    Chef::Log.info("Interconnect already created in #{enclosure} at bay #{bay_number}")
+  end
+  do_add = proc do |bay, enc|
+    converge_by "Add interconnect in #{enclosure} at bay #{bay_number}" do
+      item.create(bay, enc)
     end
   end
-  interconnect_handler(noaction, do_create)
+  interconnect_handler(noaction, do_add, item)
 end
 
-action :delete_interconnect do
-  do_delete = do
-    converge_by "Delete interconnect in #{enclosure} at bay #{bay_number}" do
-      item.delete(bay_number, enclosure_item)
+action :remove_interconnect do
+  item = load_resource
+  do_remove = proc do |bay, enc|
+    converge_by "Remove interconnect in #{enclosure} at bay #{bay_number}" do
+      item.delete(bay, enc)
     end
   end
-  noaction = { Chef::Log.info("Interconnect not present in #{enclosure} at bay #{bay_number}") }
-  interconnect_handler(do_delete, noaction)
+  noaction = proc do
+    Chef::Log.info("Interconnect not present in #{enclosure} at bay #{bay_number}")
+  end
+  interconnect_handler(do_remove, noaction, item)
 end
 
 action :update_internal_networks do
@@ -112,8 +116,17 @@ action :update_ethernet_settings do
   update_handler(:update_ethernet_settings, 'ethernetSettings')
 end
 
+action :rogue do
+  item = load_resource
+  item.retrieve!
+  item['ethernetSettings']['igmpIdleTimeoutInterval'] = 750
+  converge_by 'Action update_ethernet_settings test' do
+    item.update_ethernet_settings
+  end
+end
+
 action :update_port_monitor do
-  update_handler(:update_ethernet_settings, 'ethernetSettings')
+  update_handler(:update_ethernet_settings, 'portMonitor')
 end
 
 action :update_qos_configuration do
@@ -126,6 +139,10 @@ end
 
 action :update_snmp_configuration do
   update_handler(:update_snmp_configuration, 'snmpConfiguration')
+end
+
+action :update_firmware do
+  # TODO
 end
 
 action :update_from_group do
