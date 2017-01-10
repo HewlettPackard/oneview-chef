1c739cb7a104802b8a9e8df7408349a2962359b1
Jared Smartt <jared.smartt@hpe.com>
2017-01-10 15:51:44 +0000

    Fixed rubocop issues

--- spec/unit/resources/ethernet_network/reset_connection_template_spec.rb
+++ spec/unit/resources/ethernet_network/reset_connection_template_spec.rb
@@ -4,12 +4,14 @@ describe 'oneview_test::ethernet_network_reset_connection_template' do
   let(:resource_name) { 'ethernet_network' }
   include_context 'chef context'
 
-  let(:default_bandwidth) {{
-    'bandwidth' => {
-      'maximumBandwidth' => 10000,
-      'typicalBandwidth' => 2500
+  let(:default_bandwidth) do
+    {
+      'bandwidth' => {
+        'maximumBandwidth' => 10_000,
+        'typicalBandwidth' => 2500
+      }
     }
-  }}
+  end
 
   it 'updates it when it exists but not alike' do
     allow_any_instance_of(OneviewSDK::EthernetNetwork).to receive(:retrieve!).and_return(true)
