83f2d4fabab89bda52202fcdd2f62aa7ec60a9fe
Thiago Miotto <tmiotto@hpe.com>
2017-01-20 10:03:15 -0200

    API300 Storage System

--- spec/unit/resources/storage_system/edit_credentials_spec.rb
+++ spec/unit/resources/storage_system/edit_credentials_spec.rb
@@ -5,13 +5,12 @@ describe 'oneview_test::storage_system_edit_credentials' do
   include_context 'chef context'
 
   it 'does nothing when storage system does not exists' do
-    allow_any_instance_of(OneviewSDK::StorageSystem).to receive(:exists?).and_return(false)
+    allow_any_instance_of(OneviewSDK::StorageSystem).to receive(:retrieve!).and_return(false)
     expect_any_instance_of(OneviewSDK::StorageSystem).to_not receive(:update).and_return(true)
     expect(real_chef_run).to edit_oneview_storage_system_credentials('StorageSystem1')
   end
 
   it 'edits storage system credentials when it exists' do
-    allow_any_instance_of(OneviewSDK::StorageSystem).to receive(:exists?).and_return(true)
     allow_any_instance_of(OneviewSDK::StorageSystem).to receive(:retrieve!).and_return(true)
     expect_any_instance_of(OneviewSDK::StorageSystem).to receive(:update).and_return(true)
     expect(real_chef_run).to edit_oneview_storage_system_credentials('StorageSystem1')
