7a2080dc82fed1f031ef6f16af584c682f971295
Thiago Miotto <tmiotto@hpe.com>
2017-01-26 16:30:44 -0200

    API300 Volume

--- resources/volume.rb
+++ resources/volume.rb
@@ -19,109 +19,22 @@ property :snapshot_data, Hash
 
 default_action :create
 
-action_class do
-  include OneviewCookbook::Helper
-  include OneviewCookbook::ResourceBase
-
-  # Loads the Volume with all the external resources (if needed)
-  # @return [OneviewSDK::Volume] Loaded Volume resource
-  # rubocop:disable Metrics/MethodLength
-  def load_resource_with_associated_resources
-    item = load_resource
-    if volume_template
-      item.set_storage_volume_template(OneviewSDK::VolumeTemplate.new(item.client, name: volume_template))
-    else # Can't set the storage_pool or snapshot_pool if we specify a volume_template
-      load_storage_system(item) if storage_system
-
-      # item.set_storage_pool(OneviewSDK::StoragePool.new(item.client, name: storage_pool)) if storage_pool
-      # Workaround for issue in oneview-sdk:
-      if storage_pool
-        raise 'Must specify a storage_system to use the storage_pool helper.' unless item['storageSystemUri']
-        sp = OneviewSDK::StoragePool.find_by(item.client, name: storage_pool, storageSystemUri: item['storageSystemUri']).first
-        raise "Storage Pool '#{sp['name']}' not found" unless sp
-        item['storagePoolUri'] = sp['uri']
-      end
-
-      if snapshot_pool
-        raise 'Must specify a storage_system to use the storage_pool helper.' unless item['storageSystemUri']
-        snapshot_pool_resource = OneviewSDK::StoragePool.find_by(item.client, name: snapshot_pool, storageSystemUri: item['storageSystemUri']).first
-        item.set_snapshot_pool(snapshot_pool_resource)
-      end
-    end
-
-    # Convert capacity integers to strings
-    item['provisionedCapacity'] = item['provisionedCapacity'].to_s if item['provisionedCapacity']
-    item['allocatedCapacity'] = item['allocatedCapacity'].to_s if item['allocatedCapacity']
-
-    unless item.exists? # Also set provisioningParameters if the volume does not exist
-      item['provisioningParameters'] ||= {}
-      item['provisioningParameters']['shareable'] = item['shareable'] if !item['shareable'].nil? && item['provisioningParameters']['shareable'].nil?
-      item['provisioningParameters']['provisionType'] ||= item['provisionType'] if item['provisionType']
-      item['provisioningParameters']['requestedCapacity'] ||= item['provisionedCapacity'] if item['provisionedCapacity']
-      item['provisioningParameters']['storagePoolUri'] ||= item['storagePoolUri'] if item['storagePoolUri']
-      item.data.delete('provisioningParameters') if item['provisioningParameters'].empty?
-    end
-    item
-  end
-  # rubocop:enable Metrics/MethodLength
-
-  # Loads Storage System into the given Volume resource.
-  # The storage_system property needs to be set to a name or IP in order to use this method
-  # @param [OneviewSDK::Volume] item Volume to add the Storage System
-  # @return [OneviewSDK::Volume] Volume with Storage System parameters updated
-  def load_storage_system(item)
-    storage_system_resource = OneviewSDK::StorageSystem.new(item.client, credentials: { ip_hostname: storage_system })
-    unless storage_system_resource.exists?
-      storage_system_resource = OneviewSDK::StorageSystem.new(item.client, name: storage_system)
-    end
-    item.set_storage_system(storage_system_resource)
-  end
-end
-
 action :create do
-  create_or_update(load_resource_with_associated_resources)
+  OneviewCookbook::Helper.do_resource_action(self, :Volume, :create_or_update)
 end
 
 action :create_if_missing do
-  create_if_missing(load_resource_with_associated_resources)
+  OneviewCookbook::Helper.do_resource_action(self, :Volume, :create_if_missing)
 end
 
 action :delete do
-  delete
+  OneviewCookbook::Helper.do_resource_action(self, :Volume, :delete)
 end
 
 action :create_snapshot do
-  item = load_resource
-  raise "Unspecified property: 'snapshot_data'. Please set it before attempting this action." unless snapshot_data
-  raise "Resource not found: #{resource_name} '#{item['name']}'" unless item.exists?
-
-  temp = convert_keys(snapshot_data, :to_s)
-  item.retrieve!
-  snapshot = item.get_snapshot(temp['name'])
-  if snapshot.empty?
-    Chef::Log.info "Creating oneview_volume '#{name}' snapshot"
-    converge_by "Created oneview_volume '#{name}' snapshot" do
-      item.create_snapshot(temp)
-    end
-  else
-    Chef::Log.info "Volume snapshot '#{temp['name']}' already exists"
-  end
+  OneviewCookbook::Helper.do_resource_action(self, :Volume, :create_snapshot)
 end
 
 action :delete_snapshot do
-  item = load_resource
-  raise "Unspecified property: 'snapshot_data'. Please set it before attempting this action." unless snapshot_data
-  raise "Resource not found: #{resource_name} '#{item['name']}'" unless item.exists?
-
-  temp = convert_keys(Marshal.load(Marshal.dump(snapshot_data)), :to_s)
-  item.retrieve!
-  snapshot = item.get_snapshot(temp['name'])
-  if snapshot.empty?
-    Chef::Log.info "Volume snapshot '#{temp['name']}' is already deleted"
-  else
-    Chef::Log.info "Deleting oneview_volume '#{name}'"
-    converge_by "Deleted oneview_volume_snapshot '#{name}'" do
-      item.delete_snapshot(temp['name'])
-    end
-  end
+  OneviewCookbook::Helper.do_resource_action(self, :Volume, :delete_snapshot)
 end
