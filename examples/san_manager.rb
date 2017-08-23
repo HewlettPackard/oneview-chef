8b63151976695a9a16d063340cd099cd289d85b0
Alex Monteiro <aalexmonteiro@gmail.com>
2017-08-23 21:22:14 +0000

    Adding san manager api500 support

--- examples/san_manager.rb
+++ examples/san_manager.rb
@@ -32,7 +32,7 @@ oneview_san_manager '172.18.15.1' do
 end
 
 # Example: remove the SAN manager
-oneview_san_manager '172.18.15.1' do
+oneview_san_manager 'Remove 172.18.15.1' do
   client my_client
   action :remove
 end
