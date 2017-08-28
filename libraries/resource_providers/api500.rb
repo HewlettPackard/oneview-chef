c91333eb5a3c545644c3780badcb8a316dbcc0e7
Thiago Miotto <tmiotto@hpe.com>
2017-08-28 15:22:23 -0300

    Added changes to API500 resources

--- libraries/resource_providers/api500.rb
+++ libraries/resource_providers/api500.rb
@@ -9,6 +9,8 @@
 # CONDITIONS OF ANY KIND, either express or implied. See the License for the
 # specific language governing permissions and limitations under the License.
 
+require_relative 'api300'
+
 module OneviewCookbook
   # Module for Oneview API 500 Resources
   module API500
