71ff467f3087593be6eae0b79135e8ec7a7d7ec9
Alex Monteiro <aalexmonteiro@gmail.com>
2017-08-29 19:36:24 +0000

    Removing unnecessary require in firmware_driver_provider file

--- libraries/resource_providers/api500/c7000/firmware_driver_provider.rb
+++ libraries/resource_providers/api500/c7000/firmware_driver_provider.rb
@@ -9,8 +9,6 @@
 # CONDITIONS OF ANY KIND, either express or implied. See the License for the
 # specific language governing permissions and limitations under the License.
 
-require_relative '../../api300/c7000/firmware_driver_provider'
-
 module OneviewCookbook
   module API500
     module C7000
