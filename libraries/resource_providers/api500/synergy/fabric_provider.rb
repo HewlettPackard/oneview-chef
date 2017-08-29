bc8bb77ba0aea770b244b751c8b76ccf411351eb
Alex Monteiro <aalexmonteiro@gmail.com>
2017-08-29 19:52:17 +0000

    Removing unnecessary require in fabric_provider file

--- libraries/resource_providers/api500/synergy/fabric_provider.rb
+++ libraries/resource_providers/api500/synergy/fabric_provider.rb
@@ -1,4 +1,4 @@
-# (c) Copyright 2016 Hewlett Packard Enterprise Development LP
+# (c) Copyright 2017 Hewlett Packard Enterprise Development LP
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -8,8 +8,6 @@
 # under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
 # CONDITIONS OF ANY KIND, either express or implied. See the License for the
 # specific language governing permissions and limitations under the License.
-
-require_relative '../../api300/synergy/fabric_provider'
 
 module OneviewCookbook
   module API500
