885cbf2463be6a060ef45a8b921ca0542a9ded4a
Ricardo Galeno <ricardogaleno.rg@gmail.com>
2017-09-05 16:49:32 -0300

    Adding shared examples to run tests of scopes actions

---
+++ spec/unit/shared_examples/add_to_scopes.rb
@@ -0,0 +1,50 @@
+# (C) Copyright 2017 Hewlett Packard Enterprise Development LP
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# You may not use this file except in compliance with the License.
+# You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed
+# under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+# CONDITIONS OF ANY KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations under the License.
+
+# NOTE:
+# This shared examples needs of below variables:
+#  target_class - The full name of Oneview resource target of the test
+#  scope_class - The full name of Oneview Scope resource used in the test
+#  target_match_method - Array with name of match method called and with the argument of the match method,
+#    e.g: let(:target_match_method) { [:add_oneview_enclosure_to_scopes, 'EnclosureName'] }
+
+RSpec.shared_examples 'action :add_to_scopes' do
+  let(:scope1) { scope_class.new(client, name: 'Scope1', uri: '/rest/fake/1') }
+  let(:scope2) { scope_class.new(client, name: 'Scope2', uri: '/rest/fake/2') }
+
+  before do
+    allow(scope_class).to receive(:new).and_return(scope1, scope2)
+    allow(scope1).to receive(:retrieve!).and_return(true)
+    allow(scope2).to receive(:retrieve!).and_return(true)
+    allow_any_instance_of(target_class).to receive(:retrieve!).and_return(true)
+    allow_any_instance_of(target_class).to receive(:[]).and_call_original
+  end
+
+  it 'adds all scopes when are not added' do
+    allow_any_instance_of(target_class).to receive(:[]).with('scopeUris').and_return([])
+    expect_any_instance_of(target_class).to receive(:add_scope).with(scope1)
+    expect_any_instance_of(target_class).to receive(:add_scope).with(scope2)
+    expect(real_chef_run).to send(*target_match_method)
+  end
+
+  it 'adds only the scope that is not added' do
+    allow_any_instance_of(target_class).to receive(:[]).with('scopeUris').and_return(['/rest/fake/1'])
+    expect_any_instance_of(target_class).not_to receive(:add_scope).with(scope1)
+    expect_any_instance_of(target_class).to receive(:add_scope).with(scope2)
+    expect(real_chef_run).to send(*target_match_method)
+  end
+
+  it 'does nothing when scopes are already added' do
+    allow_any_instance_of(target_class).to receive(:[]).with('scopeUris').and_return(['/rest/fake/1', '/rest/fake/2'])
+    expect_any_instance_of(target_class).not_to receive(:add_scope)
+    expect(real_chef_run).to send(*target_match_method)
+  end
+end
---
+++ spec/unit/shared_examples/pacth.rb
@@ -0,0 +1,24 @@
+# (C) Copyright 2017 Hewlett Packard Enterprise Development LP
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# You may not use this file except in compliance with the License.
+# You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed
+# under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+# CONDITIONS OF ANY KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations under the License.
+
+# NOTE:
+# This shared examples needs of below variables:
+#  target_class - The full name of Oneview resource target of the test
+#  target_match_method - Array with name of match method called and with the argument of the match method,
+#    e.g: let(:target_match_method) { [:add_oneview_enclosure_to_scopes, 'EnclosureName'] }
+
+RSpec.shared_examples 'action :patch' do
+  it 'performs patch operation' do
+    expect_any_instance_of(target_class).to receive(:retrieve!).and_return(true)
+    expect_any_instance_of(target_class).to receive(:patch).with('test', 'test/', 'TestMessage').and_return(true)
+    expect(real_chef_run).to send(*target_match_method)
+  end
+end
