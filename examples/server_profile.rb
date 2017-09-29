e3bfd704a695e9cf1de926c96ece0de16897c13e
Ricardo Galeno <ricardogaleno.rg@gmail.com>
2017-09-29 11:09:36 -0300

    Refactoring the examples of volume attachments

--- examples/server_profile.rb
+++ examples/server_profile.rb
@@ -9,58 +9,20 @@
 # CONDITIONS OF ANY KIND, either express or implied. See the License for the
 # specific language governing permissions and limitations under the License.
 
+# NOTES:
+#  - It is needed configure previously some things like the network connections, storage pool, storage system, etc., to run this example.
+#  - The attributes file "volume_attachments_variables" is loading variables to be used in this example.
+
 my_client = {
   url: ENV['ONEVIEWSDK_URL'],
   user: ENV['ONEVIEWSDK_USER'],
   password: ENV['ONEVIEWSDK_PASSWORD']
 }
 
-# my_server_profile_template = 'spt1'
-# my_server_hardware_type = 'SY 480 Gen9 2'
-# my_server_hardware = '0000A66101, bay 5'
-# my_enclosure_group = 'eg1'
 my_server_profile_template = 'spt1'
 my_server_hardware_type = 'BL660c Gen9 1'
 my_server_hardware = 'Encl1, bay 2'
 my_enclosure_group = 'EG_1'
-
-# use this to create server profile with volume attachments using api200 or api300
-volume_data_api_200 = {
-  name: 'Volume1',
-  description: 'volume for api200 or api300',
-  provisioningParameters: {
-    provisionType: 'Full',
-    requestedCapacity: 1024 * 1024 * 1024,
-    shareable: false
-  }
-}
-
-# use this to create server profile with volume attachments using api500 or greater
-volume_data_api_500 = {
-  name: 'Volume1',
-  description: 'Volume store serv for api500',
-  size: 1024 * 1024 * 1024,
-  provisioningType: 'Thin',
-  isShareable: false
-}
-
-# use this to add storage paths to volume attachments using api200 or api300
-storage_paths_api_200 = [
-  {
-    isEnabled: true,
-    storageTargetType: 'Auto',
-    connectionId: 1
-  }
-]
-
-# use this to add storage paths to volume attachments using api500 or greater
-storage_paths_api500 = [
-  {
-    isEnabled: true,
-    targetSelector: 'Auto',
-    connectionId: 1
-  }
-]
 
 # Creates a server profile with the desired Enclosure group and Server hardware type
 oneview_server_profile 'ServerProfile1' do
@@ -179,22 +141,22 @@ oneview_server_profile 'ServerProfile4' do
   volume_attachments(
     [
       {
-        volume: 'test2',
+        volume: 'Volume2',
         attachment_data: {
           id: 1,
           lunType: 'Auto',
-          storagePaths: storage_paths_api_200
+          storagePaths: node.run_state[:storage_paths_for_volume_attachment]
         }
       },
       {
-        volume_data: volume_data_api_200,
+        volume_data: node.run_state[:volume_data_for_volume_attachment],
         storage_system: 'ThreePAR-1',
         storage_pool: 'cpg-growth-limit-1TiB',
         host_os_type: 'Windows 2012 / WS2012 R2',
         attachment_data: {
           id: 2,
           lunType: 'Auto',
-          storagePaths: storage_paths_api_200
+          storagePaths: node.run_state[:storage_paths_for_volume_attachment]
         }
       }
     ]
--- examples/server_profile_template.rb
+++ examples/server_profile_template.rb
@@ -9,8 +9,9 @@
 # CONDITIONS OF ANY KIND, either express or implied. See the License for the
 # specific language governing permissions and limitations under the License.
 
-# NOTE:
-#  It is needed configure previously some things like the network connections, storage pool, storage system, etc., to run this example.
+# NOTES:
+#  - It is needed configure previously some things like the network connections, storage pool, storage system, etc., to run this example.
+#  - The attributes file "volume_attachments_variables" is loading variables to be used in this example.
 
 my_client = {
   url: ENV['ONEVIEWSDK_URL'],
@@ -21,44 +22,6 @@ my_client = {
 enclosure_group = 'EG_1'
 server_hardware_type = 'BL660c Gen9 1'
 sp_name = 'sp1'
-
-# use this to create server profile template with volume attachments using api200 or api300
-volume_data_api_200 = {
-  name: 'Volume1',
-  description: 'volume for api200 or api300',
-  provisioningParameters: {
-    provisionType: 'Full',
-    requestedCapacity: 1024 * 1024 * 1024,
-    shareable: false
-  }
-}
-
-# use this to create server profile template with volume attachments using api500 or greater
-volume_data_api_500 = {
-  name: 'Volume1',
-  description: 'Volume store serv for api500',
-  size: 1024 * 1024 * 1024,
-  provisioningType: 'Thin',
-  isShareable: false
-}
-
-# use this to add storage paths to volume attachments using api200 or api300
-storage_paths_api_200 = [
-  {
-    isEnabled: true,
-    storageTargetType: 'Auto',
-    connectionId: 1
-  }
-]
-
-# use this to add storage paths to volume attachments using api500 or greater
-storage_paths_api_500 = [
-  {
-    isEnabled: true,
-    targetSelector: 'Auto',
-    connectionId: 1
-  }
-]
 
 # Creates on server profile template with the desired Enclosure group and Server hardware type
 oneview_server_profile_template 'ServerProfileTemplate1' do
@@ -76,22 +39,22 @@ oneview_server_profile_template 'ServerProfileTemplate1' do
   volume_attachments(
     [
       {
-        volume: 'test2',
+        volume: 'Volume2',
         attachment_data: {
           id: 1,
           lunType: 'Auto',
-          storagePaths: storage_paths_api_200
+          storagePaths: node.run_state[:storage_paths_for_volume_attachment]
         }
       },
       {
-        volume_data: volume_data_api_200,
+        volume_data: node.run_state[:volume_data_for_volume_attachment],
         storage_system: 'ThreePAR-1',
         storage_pool: 'cpg-growth-limit-1TiB',
         host_os_type: 'Windows 2012 / WS2012 R2',
         attachment_data: {
           id: 2,
           lunType: 'Auto',
-          storagePaths: storage_paths_api_200
+          storagePaths: node.run_state[:storage_paths_for_volume_attachment]
         }
       }
     ]
