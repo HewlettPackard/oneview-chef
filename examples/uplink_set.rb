f11f048fd4057b73fb7cecb14ba9d0da63639c3f
AnushaY1916 <anuanusha471@gmail.com>
2018-05-04 15:37:12 +0530

    Api 600 support for Uplinkset

--- examples/uplink_set.rb
+++ examples/uplink_set.rb
@@ -44,7 +44,9 @@ oneview_uplink_set 'UplinkSet1' do
       {
         locationEntries:
         [
-          { value: 'MyEnclosure', type: 'Enclosure' }
+          {value: 'bay_number', type: 'Bay'},
+          { value: 'MyEnclosure', type: 'Enclosure' },
+          { value: 'port_number', type: 'Port'}
         ]
       }
     ]
@@ -52,7 +54,7 @@ oneview_uplink_set 'UplinkSet1' do
   logical_interconnect 'LogicalInterconnect1' # Name of the associated logical interconnect
   networks ['Ethernet1', 'Ethernet2'] # Array of strings containing the name of the associated ethernet networks (can be empty - [])
   fc_networks ['FCNetwork1'] # Array of strings containing the name of the associated fc networks (can be empty - [])
-  fcoe_network ['FCoENetwork1'] # Array of strings containing the name of the associated fcoe networks (can be empty - [])
+  fcoe_networks ['FCoENetwork1'] # Array of strings containing the name of the associated fcoe networks (can be empty - [])
   native_network nil # Name of the native network (can be nil)
 end
 
