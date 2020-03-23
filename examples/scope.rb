b04d831d5ab6d19caea22c58bd1507753c357c63
Harika Chebrolu <chebroluharika@gmail.com>
2020-03-23 19:35:57 +0530

    api1200/scope fixed api version in recipes

--- examples/scope.rb
+++ examples/scope.rb
@@ -15,7 +15,7 @@ my_client = {
   url: ENV['ONEVIEWSDK_URL'],
   user: ENV['ONEVIEWSDK_USER'],
   password: ENV['ONEVIEWSDK_PASSWORD'],
-  api_version: 600
+  api_version: 1200
 }
 
 # Example: Create a Scope with a simple description
