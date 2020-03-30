ac0e88687cdc9ec86541013bc68afac89a9ed58f
Chebrolu Bala Sai Harika <34155580+chebroluharika@users.noreply.github.com>
2020-03-30 13:19:48 +0530

    api1200/deploymentplan update resourcefile to update apiversion to 1020

--- libraries/resource_providers/image_streamer/api1020.rb
+++ libraries/resource_providers/image_streamer/api1020.rb
@@ -34,4 +34,4 @@ module OneviewCookbook
 end
 
 # Load all API-specific resources:
-Dir[File.dirname(__FILE__) + '/api1200/*.rb'].each { |file| require file }
+Dir[File.dirname(__FILE__) + '/api1020/*.rb'].each { |file| require file }
