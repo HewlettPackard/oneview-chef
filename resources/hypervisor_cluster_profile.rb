b78531d60b2f8e9427114d880fc9dd5f0854d5b3
asisbagga <asisbagga@gmail.com>
2020-04-17 13:58:52 +0530

    ClusterProfilePR

---
+++ resources/hypervisor_cluster_profile.rb
@@ -0,0 +1,29 @@
+# (c) Copyright 2020 Hewlett Packard Enterprise Development LP
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed
+# under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+# CONDITIONS OF ANY KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations under the License.
+
+OneviewCookbook::ResourceBaseProperties.load(self)
+
+default_action :none
+
+action :none do
+end
+
+action :create do
+  OneviewCookbook::Helper.do_resource_action(self, :HypervisorClusterProfile, :create_if_missing)
+end
+
+action :update do
+  OneviewCookbook::Helper.do_resource_action(self, :HypervisorClusterProfile, :create_or_update)
+end
+
+action :delete do
+  OneviewCookbook::Helper.do_resource_action(self, :HypervisorClusterProfile, :delete)
+end
