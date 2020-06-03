f751fa383ec5addfeddf83dea9696f79817d57f8
Harika <chebroluharika@gmail.com>
2020-06-03 20:35:57 +0530

    Fix UTs

--- spec/unit/resources/server_certificate/import_certificate_spec.rb
+++ spec/unit/resources/server_certificate/import_certificate_spec.rb
@@ -3,17 +3,16 @@ require_relative './../../../spec_helper'
 describe 'oneview_test_api600_c7000::server_certificate_import_certificate' do
   let(:resource_name) { 'server_certificate' }
   include_context 'chef context'
-  let expected_output = {
-        'aliasName' => '172.18.13.11',
-        'remoteIp' => '172.18.13.11',
-        'type' => 'CertificateInfoV2',
-        'certificateDetails' =>
-         [{
-           'type' => 'CertificateDetailV2',
-           'base64Data' => 'some certificate data'
-         }]
+  expected_output = {
+    'aliasName' => '172.18.13.11',
+    'remoteIp' => '172.18.13.11',
+    'type' => 'CertificateInfoV2',
+    'certificateDetails' =>
+     [{
+       'type' => 'CertificateDetailV2',
+       'base64Data' => 'some certificate data'
+     }]
   }
-
 
   it 'adds it when it does not exist' do
     expect_any_instance_of(OneviewSDK::API600::C7000::ServerCertificate).to receive(:get_certificate).and_return(expected_output)
