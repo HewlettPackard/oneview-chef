a8533f73539163efd9ccc52ee9b5713f9c7b061a
VenkateshRavula <venkatesh.ravula1992@gmail.com>
2020-07-21 11:57:58 +0000

    change UT

--- spec/unit/resources/ethernet_network/delete_bulk_spec.rb
+++ spec/unit/resources/ethernet_network/delete_bulk_spec.rb
@@ -6,7 +6,7 @@ describe 'oneview_test_api1800_c7000::ethernet_network_bulk_delete' do
   let(:target_class) { OneviewSDK::API1800::C7000::EthernetNetwork }
 
   it 'deletes it if it exist' do
-    expect_any_instance_of(target_class).to receive(:delete_bulk).and_return(true)
-    expect(real_chef_run).to bulk_delete_oneview_ethernet_network('Eth1')
+    expect_any_instance_of(target_class).to receive(:delete_bulk).and_return(target_class.new(client, {}))
+    expect(real_chef_run).to delete_bulk_oneview_ethernet_network('Eth1')
   end
 end
