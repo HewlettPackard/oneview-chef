660875613158262d7198cce9a798eb36bc0824bd
yuvirani <yuvaranisrec@gmail.com>
2020-07-24 15:54:29 +0530

    Invoke API1800 for igmp

--- spec/unit/resources/logical_interconnect/update_igmp_settings_spec.rb
+++ spec/unit/resources/logical_interconnect/update_igmp_settings_spec.rb
@@ -17,13 +17,13 @@ describe 'oneview_test::logical_interconnect_update_igmp_settings' do
 
   # Mocks the update_handler
   before(:each) do
-    allow_any_instance_of(OneviewSDK::LogicalInterconnect).to receive(:exists?).and_return(true)
-    allow_any_instance_of(OneviewSDK::LogicalInterconnect).to receive(:retrieve!).and_return(true)
-    allow_any_instance_of(OneviewSDK::LogicalInterconnect).to receive(:like?).and_return(false)
+    allow_any_instance_of(OneviewSDK::API1800::C7000::LogicalInterconnect).to receive(:exists?).and_return(true)
+    allow_any_instance_of(OneviewSDK::API1800::C7000::LogicalInterconnect).to receive(:retrieve!).and_return(true)
+    allow_any_instance_of(OneviewSDK::API1800::C7000::LogicalInterconnect).to receive(:like?).and_return(false)
   end
 
   it 'updates the igmp settings' do
-    expect_any_instance_of(OneviewSDK::LogicalInterconnect).to receive(:update_igmp_settings).and_return(true)
-    expect(real_chef_run).to update_oneview_logical_interconnect_ethernet_settings('LogicalInterconnect-update_igmp_settings')
+    expect_any_instance_of(OneviewSDK::API1800::C7000::LogicalInterconnect).to receive(:update_igmp_settings).and_return(true)
+    expect(real_chef_run).to update_oneview_logical_interconnect_igmp_settings('LogicalInterconnect-update_igmp_settings')
   end
 end
