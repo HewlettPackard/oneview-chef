bd50072d813f31ab56f2c8fd190603a1e72dde90
yuvirani <yuvaranisrec@gmail.com>
2020-09-16 05:53:31 +0000

    Add variant to client

--- examples/logical_enclosure.rb
+++ examples/logical_enclosure.rb
@@ -13,12 +13,13 @@ my_client = {
   url: ENV['ONEVIEWSDK_URL'],
   user: ENV['ONEVIEWSDK_USER'],
   password: ENV['ONEVIEWSDK_PASSWORD'],
+  variant: 'Synergy',
   api_version: 2000
 }
 
 # Example: Create a logical enclosure if it's missing
 # Available only for Synergy
-oneview_logical_enclosure 'LE1' do
+oneview_logical_enclosure 'LE' do
   client my_client
   data(
     firmwareBaselineUri: nil
@@ -26,7 +27,7 @@ oneview_logical_enclosure 'LE1' do
   enclosures ['EN1', 'EN2', 'EN3'] # List of enclosure names, serial numbers or OA IPs
   enclosure_group 'EG1'
   action :create_if_missing # This is the default action, so you don't need to specify it
-  only_if { client[:api_variant] == 'Synergy' }
+  only_if { client[:variant] == 'Synergy' }
 end
 
 # Example: Make a logical enclosure consistent with the enclosure group
@@ -51,7 +52,7 @@ oneview_logical_enclosure 'LE1' do
   client my_client
   script '# My script commands here'
   action :set_script
-  only_if { client[:api_variant] == 'C7000' }
+  only_if { client[:variant] == 'C7000' }
 end
 
 # Example: Creates a support dump for the logical enclosure
@@ -68,5 +69,5 @@ end
 oneview_logical_enclosure 'LE1' do
   client my_client
   action :delete
-  only_if { client[:api_variant] == 'Synergy' }
+  only_if { client[:variant] == 'Synergy' }
 end
