9c542e0a83aab8de8ccff6c1a1a17bf8a0a64e40
yuvirani <yuvaranisrec@gmail.com>
2021-01-28 11:09:00 +0530

    PR for config changes #pr

---
+++ libraries/resource_providers/api2400/synergy/server_hardware_type_provider.rb
@@ -0,0 +1,20 @@
+# (c) Copyright 2020 Hewlett Packard Enterprise Development LP
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed
+# under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+# CONDITIONS OF ANY KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations under the License.
+
+module OneviewCookbook
+  module API2400
+    module Synergy
+      # ServerHardwareType API2400 Synergy provider
+      class ServerHardwareTypeProvider < OneviewCookbook::API2200::Synergy::ServerHardwareTypeProvider
+      end
+    end
+  end
+end
